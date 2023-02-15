b4f1850b9e92656d73e2f5dc398e7d0cfdf379d2
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:12:57 -0500

    Create 12.py

---
+++ lesson 1/12.py
@@ -0,0 +1,16 @@
+# Program 12: Appleland
+
+jake = 3
+mary = 5
+adam = 6
+subtotal1 = jake + mary + adam
+
+print("Jake has " + str(jake) + " apples, Mary has " + str(mary) + " apples, and Adam has " + str(adam) + " apples.", "\nIn total, they have " + str(subtotal1) + " apples.")
+
+mike = 10
+tony = 1.75
+
+subtotal2 = mike + tony
+
+print("As king, Mike had the most apples: " + str(mike) + ".", "\nIn contrast, Tony was busy eating his apples; he currently has " + str(tony) + " apples.")
+print("All together, there are " + str(subtotal1 + subtotal2) + " apples in existence.")
