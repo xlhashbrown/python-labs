ac3e1552e474d27d729329f5573438b7de73f761
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:07:43 -0500

    Create 2.py

---
+++ lesson 1/2.py
@@ -0,0 +1,11 @@
+# Program 2: print statements with end and sep
+print('My name is', 'L.') # single quotes also work
+print('L.')
+print() # an empty print statement adds a blank line
+print('My name is', 'L.', end=' ') # end adds a string at the end of every strng involved
+print('Logan Mulvey.')
+
+print('My', 'name', 'is', 'L.', sep=' - ') # sep adds a string between each string
+
+print('My birthday is', end='...')
+print('X', 'XX', 'XXXX', sep='/')
