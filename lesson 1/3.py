223173bb4d3f127973a830390fb601ea93c41001
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:08:12 -0500

    Create 3.py

---
+++ lesson 1/3.py
@@ -0,0 +1,6 @@
+# Program 3: print and escape sequences
+print("I'm")
+print("\"learning\"")
+print("\\Python\\")
+print("Hello! My name is... \nL.")
+print("name: L \t address: 123 Jefferson Avenue\t city: Springfield \t state: Iowa \t ZIP: 67890")
