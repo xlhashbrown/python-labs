5256d38d10888575972037d8511777423ea93ca9
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:12:01 -0500

    Create 11.py

---
+++ lesson 1/11.py
@@ -0,0 +1,12 @@
+# Program 12: containment
+
+locations = ["Alaska", "Hawaii", "Iceland", "Rio"]
+quote = "I love to vacation; in fact, I travel every summer. I've been all over the world, from Rio and Egypt." # quote is just another variable
+
+print(locations)
+print(quote)
+
+print("Is Iceland in the list:", "Iceland" in locations)
+print("Is Toronto in the list:", "Toronto" in locations)
+print("Is travel in the quote:", "travel" in quote)
+print("Is Iceland in the quote:", "Iceland" in quote)
