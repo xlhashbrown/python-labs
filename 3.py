a33214d9f7ca8db269101c21fca5f64062c574f7
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:04:53 -0500

    Delete 3.py

--- 3.py
+++
@@ -1,6 +0,0 @@
-# Program 3: print and escape sequences
-print("I'm")
-print("\"learning\"")
-print("\\Python\\")
-print("Hello! My name is... \nLogan.")
-print("name: Logan \t address: 123 Jefferson Avenue\t city: Springfield \t state: Iowa \t ZIP: 67890")
