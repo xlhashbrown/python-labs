a96e733dae8e2fea1923a39fea94db17a3401d8a
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:04:24 -0500

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-# Program 1: use print statements
-print("A computer will do what you tell it to do, but that may be much different from what you had in mind.")
-print()
-print("A computer will do what you tell it to do,\n but that may be much different from what you had in mind.")
-print()
-print("A computer will do what you tell it to do," , "but that may be much different from what you had in mind." "\n-Joseph Weizenbaum")
