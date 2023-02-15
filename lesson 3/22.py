8c7478785af747961effe469a99e01ebef0ebcdf
xlhashbrown <124806900+xlhashbrown@users.noreply.github.com>
2023-02-15 12:19:40 -0500

    Create 22.py

---
+++ lesson 3/22.py
@@ -0,0 +1,6 @@
+# Program 22: console and input
+
+game = ""
+while game != "Diamond" and game != "Pearl" and game != "Platinum":
+    game = input("Enter your favorite Sinnoh Pokemon game: Diamond, Pearl, or Platinum. ")
+print("I prefer Hoenn, to be honest.")
